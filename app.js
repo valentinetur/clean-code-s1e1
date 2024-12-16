25efad56bf8312996aaa68abe5f02efe68ec100b
Valentine Tur <valentine.tur.it@gmail.com>
2024-12-16 21:29:57 +0100

    refactor: add ALTs for images  according Advanced level 1.2 in index.html and app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = 'img';
+    deleteButtonImg.alt = 'Remove button';
     deleteButton.appendChild(deleteButtonImg);
 
 
